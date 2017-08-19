e2dd6baeb2512ceaa3a80dc80192267a5a68d2ec
Nico Albers <nico.albers@aboutyou.de>
2017-08-19 16:09:57 +0200

    initial changes to forked dotfiles

--- init/20_ubuntu_apt.sh
+++ init/20_ubuntu_apt.sh
@@ -31,29 +31,28 @@ apt_packages+=(
   cowsay
   curl
   git-core
-  groff
   hollywood
   htop
-  id3tool
+  # id3tool
   imagemagick
-  jq
-  mercurial
-  nmap
-  postgresql
+  # jq   # like sed for json 
+  # mercurial
+  # nmap   # network stuff
+  # postgresql
   python-pip
-  silversearcher-ag
-  sl
+  # silversearcher-ag   # some code-searching tool
+  sl   # trolling on spelling mistake for ls
   telnet
-  tree
+  tree   # recursive tree for folder
 )
 
 apt_packages+=(vim)
 is_ubuntu_desktop && apt_packages+=(vim-gnome)
 
-# https://launchpad.net/~stebbins/+archive/ubuntu/handbrake-releases
-add_ppa ppa:stebbins/handbrake-releases
-apt_packages+=(handbrake-cli)
-is_ubuntu_desktop && apt_packages+=(handbrake-gtk)
+# # https://launchpad.net/~stebbins/+archive/ubuntu/handbrake-releases
+# add_ppa ppa:stebbins/handbrake-releases
+# apt_packages+=(handbrake-cli)
+# is_ubuntu_desktop && apt_packages+=(handbrake-gtk)
 
 # https://github.com/rbenv/ruby-build/wiki
 apt_packages+=(
@@ -66,11 +65,11 @@ add_ppa ppa:ansible/ansible
 apt_packages+=(ansible)
 
 if is_ubuntu_desktop; then
-  # http://www.omgubuntu.co.uk/2016/06/install-latest-arc-gtk-theme-ubuntu-16-04
-  apt_keys+=(http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key)
-  apt_source_files+=(arc-theme)
-  apt_source_texts+=("deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /")
-  apt_packages+=(arc-theme)
+#   # http://www.omgubuntu.co.uk/2016/06/install-latest-arc-gtk-theme-ubuntu-16-04
+#   apt_keys+=(http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key)
+#   apt_source_files+=(arc-theme)
+#   apt_source_texts+=("deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /")
+#   apt_packages+=(arc-theme)
 
   # https://www.ubuntuupdates.org/ppa/google_chrome
   apt_keys+=(https://dl-ssl.google.com/linux/linux_signing_key.pub)
@@ -78,17 +77,17 @@ if is_ubuntu_desktop; then
   apt_source_texts+=("deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main")
   apt_packages+=(google-chrome-stable)
 
-  # https://www.charlesproxy.com/documentation/installation/apt-repository/
-  apt_keys+=(https://www.charlesproxy.com/packages/apt/PublicKey)
-  apt_source_files+=(charles)
-  apt_source_texts+=("deb https://www.charlesproxy.com/packages/apt/ charles-proxy3 main")
-  apt_packages+=(charles-proxy)
+  # # https://www.charlesproxy.com/documentation/installation/apt-repository/
+  # apt_keys+=(https://www.charlesproxy.com/packages/apt/PublicKey)
+  # apt_source_files+=(charles)
+  # apt_source_texts+=("deb https://www.charlesproxy.com/packages/apt/ charles-proxy3 main")
+  # apt_packages+=(charles-proxy)
 
-  # https://github.com/aluxian/Messenger-for-Desktop#linux
-  apt_keys+=('--keyserver pool.sks-keyservers.net --recv 6DDA23616E3FE905FFDA152AE61DA9241537994D')
-  apt_source_files+=(aluxian)
-  apt_source_texts+=("deb https://dl.bintray.com/aluxian/deb/ beta main")
-  apt_packages+=(messengerfordesktop)
+  # # https://github.com/aluxian/Messenger-for-Desktop#linux
+  # apt_keys+=('--keyserver pool.sks-keyservers.net --recv 6DDA23616E3FE905FFDA152AE61DA9241537994D')
+  # apt_source_files+=(aluxian)
+  # apt_source_texts+=("deb https://dl.bintray.com/aluxian/deb/ beta main")
+  # apt_packages+=(messengerfordesktop)
 
   # https://www.spotify.com/us/download/linux/
   apt_keys+=('--keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886')
@@ -117,44 +116,44 @@ if is_ubuntu_desktop; then
     echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
   }
 
-  # https://github.com/colinkeenan/silentcast/#ubuntu
-  # https://github.com/colinkeenan/silentcast/#ubuntu-linux-full-install
-  add_ppa ppa:sethj/silentcast
-  add_ppa ppa:webupd8team/y-ppa-manager
-  apt_packages+=(
-    libav-tools x11-xserver-utils xdotool wininfo wmctrl python-gobject python-cairo xdg-utils yad
-    silentcast
-  )
+  # # https://github.com/colinkeenan/silentcast/#ubuntu
+  # # https://github.com/colinkeenan/silentcast/#ubuntu-linux-full-install
+  # add_ppa ppa:sethj/silentcast
+  # add_ppa ppa:webupd8team/y-ppa-manager
+  # apt_packages+=(
+  #   libav-tools x11-xserver-utils xdotool wininfo wmctrl python-gobject python-cairo xdg-utils yad
+  #   silentcast
+  # )
 
   # Misc
-  apt_packages+=(adb fastboot)
+  # apt_packages+=(adb fastboot)
   apt_packages+=(
-    chromium-browser
-    fonts-mplus
-    k4dirstat
-    rofi
+    # chromium-browser
+    # fonts-mplus
+    # k4dirstat
+    # rofi
     network-manager-openconnect
     network-manager-openconnect-gnome
-    openssh-server
-    shutter
-    transgui
+    # openssh-server
+    # shutter
+    # transgui
     unity-tweak-tool
     vlc
     xclip
-    zenmap
+    # zenmap
   )
 
   # Manage online accounts via "gnome-control-center" in launcher
   apt_packages+=(gnome-control-center gnome-online-accounts)
 
-  # https://github.com/mitchellh/vagrant/issues/7411
-  deb_installed+=(/usr/bin/vagrant)
-  deb_sources+=(https://releases.hashicorp.com/vagrant/1.9.2/vagrant_1.9.2_x86_64.deb)
-  # https://github.com/vagrant-libvirt/vagrant-libvirt/issues/575
-  # apt_packages+=(vagrant)
-  # function postinstall_vagrant() {
-  #   sudo sed -i'' "s/Specification.all = nil/Specification.reset/" /usr/lib/ruby/vendor_ruby/vagrant/bundler.rb
-  # }
+  # # https://github.com/mitchellh/vagrant/issues/7411
+  # deb_installed+=(/usr/bin/vagrant)
+  # deb_sources+=(https://releases.hashicorp.com/vagrant/1.9.7/vagrant_1.9.7_x86_64.deb)
+  # # https://github.com/vagrant-libvirt/vagrant-libvirt/issues/575
+  # # apt_packages+=(vagrant)
+  # # function postinstall_vagrant() {
+  # #   sudo sed -i'' "s/Specification.all = nil/Specification.reset/" /usr/lib/ruby/vendor_ruby/vagrant/bundler.rb
+  # # }
 
   # https://launchpad.net/grub-customizer
   add_ppa ppa:danielrichter2007/grub-customizer
@@ -169,38 +168,13 @@ if is_ubuntu_desktop; then
   deb_installed+=(/usr/bin/dropbox)
   deb_sources+=("https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb")
 
-  # http://askubuntu.com/a/852727
-  apt_packages+=(cabextract)
-  deb_installed+=(/usr/share/fonts/truetype/msttcorefonts)
-  deb_sources+=(deb_source_msttcorefonts)
-  function deb_source_msttcorefonts() {
-    echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
-    echo http://ftp.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb
-  }
-
-  # https://slack.com/downloads/instructions/linux
-  deb_installed+=(/usr/bin/slack)
-  deb_sources+=(https://downloads.slack-edge.com/linux_releases/slack-desktop-2.5.2-amd64.deb)
-
-  # https://discordapp.com/download
-  deb_installed+=(/usr/bin/discord)
-  deb_sources+=("https://discordapp.com/api/download?platform=linux&format=deb")
-
-  # http://askubuntu.com/questions/854480/how-to-install-the-steam-client/854481#854481
-  apt_packages+=(python-apt)
-  deb_installed+=(/usr/bin/steam)
-  deb_sources+=(deb_source_steam)
-  function deb_source_steam() {
-    local steam_root steam_file
-    steam_root=http://repo.steampowered.com/steam/pool/steam/s/steam/
-    steam_file="$(wget -q -O- "$steam_root?C=M;O=D" | sed -En '/steam-launcher/{s/.*href="([^"]+)".*/\1/;p;q;}')"
-    echo "$steam_root$steam_file"
-  }
-  # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=772598
-  # apt_packages+=(steam)
-  # function preinstall_steam() {
-  #   echo steam steam/question select I AGREE | sudo debconf-set-selections
-  #   echo steam steam/license note | sudo debconf-set-selections
+  # # http://askubuntu.com/a/852727
+  # apt_packages+=(cabextract)
+  # deb_installed+=(/usr/share/fonts/truetype/msttcorefonts)
+  # deb_sources+=(deb_source_msttcorefonts)
+  # function deb_source_msttcorefonts() {
+  #   echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
+  #   echo http://ftp.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb
   # }
 fi
 
@@ -214,9 +188,9 @@ function other_stuff() {
     )
   fi
   # Install misc bins from zip file.
-  install_from_zip ngrok 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'
-  install_from_zip terraform 'https://releases.hashicorp.com/terraform/0.9.2/terraform_0.9.2_linux_amd64.zip'
-}
+#   install_from_zip ngrok 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'
+#   install_from_zip terraform 'https://releases.hashicorp.com/terraform/0.9.2/terraform_0.9.2_linux_amd64.zip'
+# }
 
 ####################
 # ACTUALLY DO THINGS

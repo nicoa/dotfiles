1c7a9c1525ebd60e7114f71f22b6a282e0f9f663
Nico Albers <nico.albers@aboutyou.de>
2017-08-20 13:36:33 +0200

    improved a little bit

--- source/50_ubuntu_desktop.sh
+++ source/50_ubuntu_desktop.sh
@@ -8,10 +8,10 @@ alias pbcopy='xclip -i -selection clipboard'
 alias pbpaste='xclip -o -selection clipboard'
 
 # http://www.omgubuntu.co.uk/2016/06/install-steam-on-ubuntu-16-04-lts
-function steam() {
-  if [[ -e ~/.steam ]]; then
-    command steam "$@"
-  else
-    LD_PRELOAD='/usr/$LIB/libstdc++.so.6 /usr/$LIB/libgcc_s.so.1 /usr/$LIB/libxcb.so.1 /usr/$LIB/libgpg-error.so' command steam
-  fi
-}
+# function steam() {
+#   if [[ -e ~/.steam ]]; then
+#     command steam "$@"
+#   else
+#     LD_PRELOAD='/usr/$LIB/libstdc++.so.6 /usr/$LIB/libgcc_s.so.1 /usr/$LIB/libxcb.so.1 /usr/$LIB/libgpg-error.so' command steam
+#   fi
+# }
